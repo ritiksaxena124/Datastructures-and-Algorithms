ea9e5f1be34dff9d13853f44d980940c651443b5
Ritik Saxena <ritiksaxena124@gmail.com>
2022-01-04 23:04:40 +0530

    Find the Next Alphabetical Element in array

---
+++ Questions/6. Binary Search/NextAlphabetical.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char next_alphabetical_element(vector<char> &arr, char target)
+{
+    char ans = '#';
+    int s = 0, e = arr.size() - 1;
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
+        if (arr[mid] == target)
+        {
+            s = mid + 1;
+        }
+        else if (arr[mid] < target)
+            s = mid + 1;
+        else
+        {
+            ans = arr[mid];
+            e = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<char> arr = {'a', 'b', 'c', 'f', 'h'};
+    char target = 'a';
+    char ans = next_alphabetical_element(arr, target);
+    if (ans == '#')
+    {
+        cout << "Current Element is the last Element.";
+    }
+    else
+        cout << ans;
+}
