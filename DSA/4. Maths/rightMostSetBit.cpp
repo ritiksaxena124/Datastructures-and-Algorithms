f999971ff6c54a7266658ec5318438c8cd217c2a
Ritik Saxena <ritiksaxena124@gmail.com>
2022-02-01 00:28:05 +0530

    Find the Right Most Set Bit of a Number

---
+++ DSA/4. Maths/rightMostSetBit.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int rightMostSetBit(int n)
+{
+    /*
+    this solution will take many computations for large numbers
+    int pos = 1;
+    while(n > 0)
+    {
+        if((n & 1) == 0){
+            n = n >> 1;
+            pos++;
+        }
+        else{
+            break;
+        }
+    }
+    */
+    int pos = log2(n & -n) + 1;
+    return pos;
+}
+
+int main()
+{
+    int n = 182;
+    cout << rightMostSetBit(n);
+    return 0;
+}
